833c7cded967a7dfa29ddf6623807f80c1354483
Jon Zgoda <jon@zgoda.us>
2014-03-16 14:47:18 -0500

    Integrated dependencies into the package, and fixed a testing error.

--- package.js
+++ package.js
@@ -2,14 +2,16 @@ Package.describe({
 	summary: "A managed user extension to the base Meteor user system, with a default Admin user, and ability to add various permissions to all users."
 });
 
-Package.on_use(function(api) {
-	api.use(['minimongo', 'mongo-livedata', 'templating', 'accounts-password', 'accounts-ui', 'email'], ['client', 'server']);
+Package.on_use(function(api, where) {
+	api.use(['minimongo', 'mongo-livedata', 'templating', 'accounts-password', 'email', 'bootstrap-3', 'accounts-ui-bootstrap-3'], ['client', 'server']);
 	api.use(['handlebars'], 'client');
 	api.add_files(['managedUsers.js'], ['client', 'server']);
-	api.add_files(['managedUsersTemplates.html', 'managedUsersTemplates.js'], 'client');
+	api.add_files(['managedUsersTemplates.html', 'managedUsersTemplates.js', 'bootbox.min.js'], 'client');
+	if(api.export)
+		api.export("ManagedUsers");
 });
 
 Package.on_test(function(api) {
-	api.use(['managedUsers', 'tinytest', 'test-helpers'], ['client', 'server']);
+	api.use(['managedUsers', 'tinytest', 'test-helpers', 'accounts-password'], ['client', 'server']);
 	api.add_files('managedUsers_tests.js', ['client', 'server']);
 });
